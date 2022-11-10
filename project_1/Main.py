20cbda64f489d91b2eac4899ca303f7e095a0586
wintongee <wgee01@calpoly.edu>
2022-11-10 11:11:39 -0800

    Update

--- project_1/Main.py
+++ project_1/Main.py
@@ -1,2 +1,33 @@
-message = 'Python is fun'
-print(message)
+import random
+import math
+
+# Getting the user's input
+lower_bound = int(input("Enter Lower bound: "))
+upper_bound = int(input("Enter Upper bound: "))
+num_guesses = int(input("Enter number of guesses: "))
+
+# generate and cache a number between the lower and upper bound
+random_val = random.randint(lower_bound, upper_bound)
+print("You have ", num_guesses, " to guess the random number between ",
+      lower_bound, " and ", upper_bound)
+
+# Initializing the number of guesses.
+count = 0
+
+# for calculation of minimum number of
+# guesses depends upon range
+while count < num_guesses:
+    count += 1
+    guess = int(input("Guess a number: "))
+
+    # Checking if the player guessed the correct number.
+    if random_val == guess:
+        print("Congratulations you did it in ", count, " tries")
+        break
+    elif random_val > guess:
+        print("You guessed too small!")
+    else:
+        print("You Guessed too high!")
+
+if count >= num_guesses:
+    print("Failed to guess the random value: ", random_val, " in ", num_guesses, " attempts!")
